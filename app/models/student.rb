7669d88e2ab1473b2d660af6f90ee4c9c69a469c
HabibJR <smhabib.abir2030@gmail.com>
2022-05-22 22:31:50 +0200

    inital

--- app/models/student.rb
+++
@@ -1,6 +0,0 @@
-class Student < ApplicationRecord
-
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
 
-  get 'sign_up', to: "student#index"
-  post 'sign_up', to: "student#create"
   root 'home#index'
 end
